238bbf9606d8584e5d5959e5c7bf5fb06745aafe
skyfroger <skyfroger@gmail.com>
2018-02-26 20:44:58 +0300

    First good results

--- src/index.js
+++ src/index.js
@@ -1,26 +1,50 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.stack = [];
+    this.stack.push(initialValue);
   }
 
   add(number) {
-    // your implementation
+    this.stack.push("+" + number);
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.stack.push("-" + number);
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.stack.push("*" + number);
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.stack.push("/" + number);
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.stack.push("^" + number);
+    return this;
+  }
+
+  calculate(){
+    let str = this.stack.join('');
+    var regexp = /(\d+)((\^[0-9])+)+/g;
+    var matchesArray = str.match(regexp);
+    if(matchesArray !== null){
+      for(var i = 0; i < matchesArray.length; i++){
+        var currentRow = matchesArray[i].split('^').map((x) => (Number(x)));
+        var replacement = "Math.pow("+currentRow[0]+","+currentRow.slice(1).reduce((p, x)=>(p*x))+")";
+        str = str.replace(matchesArray[i], replacement);
+      }
+    }
+    return Number(eval(str)); // Delete Number in case of error
+  }
+
+  toString(){
+    return this.calculate();
   }
 }
 
